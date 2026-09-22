74121e8c6afbff916295b25538a1435aa6d641fe
zhangjiankang <15378790060@163.com>
2026-09-22 17:46:29 +0800

    feat(hello): add Cloudflare Pages Support!

--- client/functions/api/hello.js
+++ client/functions/api/hello.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-    return new Response(JSON.stringify({ message: "Hello from Cloudflare Pages!freellmapi" }), {
+    return new Response(JSON.stringify({ message: "Hello from Cloudflare Pages!freellmapi client" }), {
         headers: { "Content-Type": "application/json" },
     });
 }
