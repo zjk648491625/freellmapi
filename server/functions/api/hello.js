84b4101fdf9baa350b08ad06c9b79aa000499c3c
zhangjiankang <15378790060@163.com>
2026-09-22 15:17:36 +0800

    feat(hello): 添加 Cloudflare Pages 的请求处理功能

--- server/functions/api/hello.js
+++ server/functions/api/hello.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
-    return new Response(JSON.stringify({ message: "Hello from Cloudflare Pages!" }), {
+    return new Response(JSON.stringify({ message: "Hello from Cloudflare Pages!freellmapi" }), {
         headers: { "Content-Type": "application/json" },
     });
 }
